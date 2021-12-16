842fd7a6f7cab3d5ea36795dce7965569a50b601
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2021-12-16 10:32:49 +0530

    Create week6que.c

---
+++ week6que.c
@@ -0,0 +1,110 @@
+#task2
+
+Q1:-We have discussed Queue operations called insert() and delete() in the class. 
+Also illustrated with examples. Design and implement menu driven C program with 4 operations. 
+(1) Add element to Queue (2) Delete element from Queue (3) Traverse elements and (4) Exit. 
+Write all possible examples supported by relevant test cases. 
+
+  
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+void insert(int );
+void delete();
+int front=0;
+int rear=0;
+int max_size=10;
+int a[10];
+void insert(int x)
+{
+    
+    if(rear>max_size)
+    {
+        printf("Queue Overflow");
+    }
+    else
+    {
+     
+     rear++;
+     a[rear]=x;
+    }
+}
+void delete()
+{
+    int x;
+    if (front==rear || rear==0)
+    {
+        printf("Queue Empty");
+        
+    }
+    else
+    {
+        front++;
+        x=a[front];
+        printf("Deleted element is:%d",x);
+        a[front]='\0';
+        free(a[x]);
+       
+        
+    }
+    
+}
+void display()
+{
+    int i;
+    if(rear<=0)
+    {
+        printf("Queue empty");
+    }
+    else{
+    for(i=1;i<=rear;i++)
+    {
+    printf("%d ",a[i]);
+    }
+}
+}
+int main()
+{
+    int dec,x; 
+    
+    
+    
+    while(1)
+    {
+        printf("\n\t\t\tWELCOME TO AJAY MENU DRIVEN PROGRAM\t\t\t");
+        printf("\nSelect any one option from below options:\n");
+        printf("1)insert Element to queue\n2)Delete an Element\n3)Display an Element\n4)exit\n");
+        scanf("%d",&dec);
+        switch(dec)
+        {
+            case 1:
+            {
+               printf("Enter an element to Insert:");
+               scanf("%d",&x);   
+               insert(x);
+               break;
+            }
+            case 2:
+            {   
+                
+                delete();
+                
+                break;
+            }
+            case 3:
+            {
+                display();
+                break;
+            }
+            case 4:
+            {
+                exit(0);
+                break;
+            }
+        }
+    }
+    return 0;
+}
+
+-----------------------------------------------------------------------------------------------------------------------
+  
