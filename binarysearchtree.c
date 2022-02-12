81e98eaf99c535c0bfcf69b52beeb8403c7ce003
N.AJAY KUMAR VARMA <90514443+ajaykumarvarma@users.noreply.github.com>
2022-02-12 11:39:31 +0530

    Update binarysearchtree.c

--- binarysearchtree.c
+++ binarysearchtree.c
@@ -1,136 +1,264 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct node
+#include <stdio.h>
+#include <stdlib.h>
+struct btnode
+{
+    int value;
+    struct btnode *l;
+    struct btnode *r;
+}*root = NULL, *temp = NULL, *t2, *t1;
+void delete1();
+void insert();
+void delete();
+void inorder(struct btnode *t);
+void create();
+void search(struct btnode *t);
+void preorder(struct btnode *t);
+void postorder(struct btnode *t);
+void search1(struct btnode *t,int data);
+int smallest(struct btnode *t);
+int largest(struct btnode *t);
+int flag = 1;
+void insert()
+{
+    create();
+    if (root == NULL) 
+        root = temp;
+    else    
+        search(root);    
+}
+
+void create()
 {
     int data;
-    struct node *left,*right;
-    
-};
-struct node *addnewnode(int val)
-{
-    struct node *temp = (struct node *)malloc(sizeof(struct node *));
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    return temp;
-}
-struct node *insertelement(struct node *node, int data)
-{
-    if (node == NULL)
-    {
-        return addnewnode(data);
-    }
-
-    if (data < node->data)
-    {
-        node->left = insertelement(node->left, data);
-    }
-    else if (data > node->data)
-    {
-        node->right = insertelement(node->right, data);
-    }
-
-    return node;
-}
-void inorder(struct node *root) 
-{
-    if (root != NULL)
-    {
-        inorder(root->left);
-        printf("%d \n", root->data);
-        inorder(root->right);
-    }
-}
-void postorder(struct node *root) 
-{
-    if (root != NULL)
-    {
-        inorder(root->left);
-        inorder(root->right);
-        printf("%d \n", root->data);
-    }
-}
-void preorder(struct node *root) 
-{
-    if (root != NULL)
-    {
-        printf("%d \n", root->data);
-        inorder(root->left);
-        inorder(root->right);
-        
-    }
+    printf("Enter node to be inserted : ");
+    scanf("%d", &data);
+    temp = (struct btnode *)malloc(1*sizeof(struct btnode));
+    temp->value = data;
+    temp->l = temp->r = NULL;
 }
 
-struct node *minimumvalue(struct node *node){
-
-    struct node *currentvalue = node;
-
-    while (currentvalue && currentvalue->left != NULL){
-    
-        currentvalue = currentvalue->left;
-    }
-
-    return currentvalue;
-}
-struct node *deletenode(struct node *root, int data)
+void search(struct btnode *t)
 {
-    if (root == NULL){
-        return root;
-    }
+    if ((temp->value > t->value) && (t->r != NULL))      
+        search(t->r);
+    else if ((temp->value > t->value) && (t->r == NULL))
+        t->r = temp;
+    else if ((temp->value < t->value) && (t->l != NULL))    
+        search(t->l);
+    else if ((temp->value < t->value) && (t->l == NULL))
+        t->l = temp;
+}
 
-    if (data < root->data){
-        root->left = deletenode(root->left, data);
+void inorder(struct btnode *t)
+{
+    if (root == NULL)
+    {
+        printf("No elements in a tree to display");
+        return;
     }
-
-    else if (data > root->data){
-        root->right = deletenode(root->right, data);
+    if (t->l != NULL)    
+        inorder(t->l);
+    printf("%d -> ", t->value);
+    if (t->r != NULL)    
+        inorder(t->r);
+}
+/*deleted node */
+void delete()
+{
+    int data;
+ 
+    if (root == NULL)
+    {
+        printf("No elements in a tree to delete");
+        return;
     }
-
-    else{
-    
-        if (root->left == NULL){
-        
-            struct node *temp = root->right;
-            free(root);
-            return temp;
+    printf("Enter the node to be deleted : ");
+    scanf("%d", &data);
+    t1 = root;
+    t2 = root;
+    search1(root, data);
+}
+/*preorder traversal */
+void preorder(struct btnode *t)
+{
+    if (root == NULL)
+    {
+        printf("No elements in a tree to display");
+        return;
+    }
+    printf("%d -> ", t->value);
+    if (t->l != NULL)    
+        preorder(t->l);
+    if (t->r != NULL)    
+        preorder(t->r);
+}
+/*postorder traversal */
+void postorder(struct btnode *t)
+{
+    if (root == NULL)
+    {
+        printf("No elements in a tree to display ");
+        return;
+    }
+    if (t->l != NULL) 
+        postorder(t->l);
+    if (t->r != NULL) 
+        postorder(t->r);
+    printf("%d -> ", t->value);
+}
+/*insert the new node */
+void search1(struct btnode *t, int data)
+{
+    if ((data>t->value))
+    {
+        t1 = t;
+        search1(t->r, data);
+    }
+    else if ((data < t->value))
+    {
+        t1 = t;
+        search1(t->l, data);
+    }
+    else if ((data==t->value))
+    {
+        delete1(t);
+    }
+}
+/* To delete a node */
+void delete1(struct btnode *t)
+{
+    int k;
+    /* To delete leaf node */
+    if ((t->l == NULL) && (t->r == NULL))
+    {
+        if (t1->l == t)
+        {
+            t1->l = NULL;
         }
-        else if (root->right == NULL){
-        
-            struct node *temp = root->left;
-            free(root);
-            return temp;
+        else
+        {
+            t1->r = NULL;
         }
-
-        struct node *temp = minimumvalue(root->right);
-        root->data = temp->data;
-        root->right = deletenode(root->right, temp->data);
+        t = NULL;
+        free(t);
+        return;
     }
-    return root;
+    /* To delete node having one left hand child */
+    else if ((t->r == NULL))
+    {
+        if (t1 == t)
+        {
+            root = t->l;
+            t1 = root;
+        }
+        else if (t1->l == t)
+        {
+            t1->l = t->l;
+ 
+        }
+        else
+        {
+            t1->r = t->l;
+        }
+        t = NULL;
+        free(t);
+        return;
+    }
+    /* To delete node having right hand child */
+    else if (t->l == NULL)
+    {
+        if (t1 == t)
+        {
+            root = t->r;
+            t1 = root;
+        }
+        else if (t1->r == t)
+            t1->r = t->r;
+        else
+            t1->l = t->r;
+        t == NULL;
+        free(t);
+        return;
+    }
+    /* To delete node having two child */
+    else if ((t->l != NULL) && (t->r != NULL))  
+    {
+        t2 = root;
+        if (t->r != NULL)
+        {
+            k = smallest(t->r);
+            flag = 1;
+        }
+        else
+        {
+            k =largest(t->l);
+            flag = 2;
+        }
+        search1(root, k);
+        t->value = k;
+    }
+ 
 }
-
-int main()
+/*smallest element in the right sub tree */
+int smallest(struct btnode *t)
 {
-    
-    struct node *root = NULL;
-    root = insertelement(root, 50);
-    insertelement(root, 10);
-    insertelement(root, 90);
-    insertelement(root, 30);
-    insertelement(root, 40);
-    insertelement(root, 20);
-    insertelement(root, 100);
-
-    
-    printf("To Delete 20:\n");
-    root = deletenode(root, 20);
-    printf("inorder traversal of the modified tree:\n");
-    inorder(root);
-    printf("Pre order is:\n");
-    preorder(root);
-    printf("Post order is:\n");
-    postorder(root);
-
-    
-
-    return 0;
+    t2 = t;
+    if (t->l != NULL)
+    {
+        t2 = t;
+        return(smallest(t->l));
+    }
+    else    
+        return (t->value);
 }
+/*largest element in the left sub tree */
+int largest(struct btnode *t)
+{
+    if (t->r != NULL)
+    {
+        t2 = t;
+        return(largest(t->r));
+    }
+    else    
+        return(t->value);
+}
+void main()
+{
+    int ch;
+    printf("\nMENU");
+    printf("\n1.Insert an element into tree:\n");
+    printf("2.Delete an element from the tree:\n");
+    printf("3.Inorder Traversal:\n");
+    printf("4.Preorder Traversal:\n");
+    printf("5.Postorder Traversal:\n");
+    printf("6.Exit\n");
+    while(1)
+    {
+        printf("\nEnter your choice : ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:    
+            insert();
+            break;
+        case 2:    
+            delete();
+            break;
+        case 3:    
+            inorder(root);
+            break;
+        case 4:    
+            preorder(root);
+            break;
+        case 5:    
+            postorder(root);
+            break;
+        case 6:    
+            exit(0);
+        default :     
+            printf("invalid choice! enter a valid option.");
+            break;    
+        }
+    }
+}
+
